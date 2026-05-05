83ef2f0ad7b33ae2a12754c86b01c77e74425fc8
Calledliuda <152193728+Calledliuda@users.noreply.github.com>
2026-05-05 13:41:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const photos = [
   {
     src: "assets/photos/1.JPG",
-    title: "第一张照片",
-    text: "这里写这张照片的故事。",
+    title: "五中教室走廊的我俩",
+    text: "2020年12月26日，在教室外面我俩依偎在一起拍滴。",
   },
   {
     src: "assets/photos/2.JPG",
-    title: "第二张照片",
-    text: "这里写这张照片的故事。",
+    title: "公交车上，仿佛看见了我们的以后",
+    text: "2021年4月30日，好像是放五一，我俩一起做公交去盘县。",
   },
   {    src: "assets/photos/3.JPG",   
-       title: "第三张照片",   
-       text: "这里写这张照片的故事。",  
+       title: "莎莎朋友圈的自拍",   
+       text: "飘飘亮亮的刘美莎～。",  
   },
   {    src: "assets/photos/4.JPG",  
-       title: "第四张照片",   
-       text: "这里写这张照片的故事。", 
+       title: "迪士尼的莎儿～",   
+       text: "我和莎莎一起去迪士尼～。", 
   },
 ];
 
